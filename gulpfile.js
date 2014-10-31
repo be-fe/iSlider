ffa1566e8787fad97a727b19f43cf9889e73dd2a
xieyu03 <xieyu03@baidu.com>
2014-10-31 11:52:26 +0800

    change gulpfile and Perspective

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp=require('gulp');
 
 gulp.task('default', function() {
    gulp.watch('src/islider.js', function(){
-        return gulp.src('src/islider.js').pipe(gulp.dest('demo/js'));
+        return gulp.src('src/islider.js').pipe(gulp.dest('demo/public/js'));
    });
 });
 
