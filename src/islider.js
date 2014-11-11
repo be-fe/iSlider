55c46df7c81e05e156305458a24abab050585965
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2014-11-11 17:59:11 +0800

    fix format issue

--- src/islider.js
+++ src/islider.js
@@ -1,5 +1,5 @@
 /**
- * iSlider 
+ * iSlider
  * A simple, efficent mobile slider
  * @Author qbatyqi
  *
@@ -7,15 +7,15 @@
  * @param {Element}     opts.dom            外层元素        Outer wrapper
  * @param {Object}      opts.data           数据列表        Content data
  * Please refer to README                   请参考README
- * @class 
+ * @class
  */
 var iSlider = function (opts) {
     if (!opts.dom) {
-        throw new Error("dom element can not be empty!");
+        throw new Error('dom element can not be empty!');
     }
 
     if (!opts.data || !opts.data.length) {
-        throw new Error("data must be an array and must have more than one element!");
+        throw new Error('data must be an array and must have more than one element!');
     }
 
     this._opts = opts;
@@ -24,37 +24,37 @@ var iSlider = function (opts) {
     this._bindHandler();
 };
 
-//setting parameters for slider
+// setting parameters for slider
 iSlider.prototype._setting = function () {
     var opts = this._opts;
 
-    //dom element wrapping pics
+    // dom element wrapping pics
     this.wrap = opts.dom;
 
-    //pics data
+    // pics data
     this.data = opts.data;
 
     //loaded image
     this.loadedImage = [];
-    
-    //default type
+
+    // default type
     this.type = opts.type || 'pic';
-    //default slide direction
+    // default slide direction
     this.isVertical = opts.isVertical || false;
 
-    //Callback function when your finger is moving
+    // Callback function when your finger is moving
     this.onslide = opts.onslide;
-    //Callback function when your finger touch the screen
+    // Callback function when your finger touch the screen
     this.onslidestart = opts.onslidestart;
-    //Callback function when the finger move out of the screen
+    // Callback function when the finger move out of the screen
     this.onslideend = opts.onslideend;
-    //Callback function when the finger move out of the screen
+    // Callback function when the finger move out of the screen
     this.onslidechange = opts.onslidechange;
-    //Slide time gap
+    // Slide time gap
     this.duration = opts.duration || 2000;
 
-    //debug mode
-    this.log = opts.isDebug ? function (str) { console.log(str) } : function (){};
+    // debug mode
+    this.log = opts.isDebug ? function(str) { console.log(str); } : function() {};
 
     this.axis = this.isVertical ? 'Y' : 'X';
     this.width = this.wrap.clientWidth;
@@ -62,35 +62,36 @@ iSlider.prototype._setting = function () {
     this.ratio = this.height / this.width;
     this.scale = opts.isVertical ? this.height : this.width;
 
-    //start from 0
+    // start from 0
     this.sliderIndex = this.sliderIndex || 0;
 
     if (this.data.length < 2) {
         this.isLooping = false;
         this.isAutoPlay = false;
-    } else {
+    } 
+    else {
         this.isLooping = opts.isLooping || false;
         this.isAutoplay = opts.isAutoplay || false;
     }
 
-    //Autoplay mode
+    // Autoplay mode
     if (this.isAutoplay) {
         this.play();
     }
 
-    //set Damping function
+    // set Damping function
     this._setUpDamping();
 
-    //set animate Function
-    this._animateFunc = (opts.animateType in this._animateFuncs) 
-    ? this._animateFuncs[opts.animateType] 
+    // set animate Function
+    this._animateFunc = (opts.animateType in this._animateFuncs)
+    ? this._animateFuncs[opts.animateType]
     : this._animateFuncs['default'];
 
-    //stop autoplay when window blur
+    // stop autoplay when window blur
     this._setPlayWhenFocus();
 };
 
-//fixed bug for android device
+// fixed bug for android device
 iSlider.prototype._setPlayWhenFocus = function() {
     var self = this;
     window.addEventListener('focus', function() {
@@ -101,9 +102,9 @@ iSlider.prototype._setPlayWhenFocus = function() {
     }, false);
 };
 
-//animate function options
+// animate function options
 /**
- * animation parmas: 
+ * animation parmas:
  *
  * @param {Element}      dom             图片的外层<li>容器       Img wrapper
  * @param {String}       axis            动画方向                animate direction
@@ -118,74 +119,83 @@ iSlider.prototype._animateFuncs = {
     },
 
     'rotate': function (dom, axis, scale, i, offset) {
-        var rotateDirect = (axis == "X") ? "Y" : "X";
+        var rotateDirect = (axis === 'X') ? 'Y' : 'X';
         var absoluteOffset = Math.abs(offset);
         var bdColor = window.getComputedStyle(this.wrap.parentNode, null).backgroundColor;
-        if (this.isVertical){ offset = -offset; }
+        if (this.isVertical) {
+            offset = -offset;
+        }
 
         this.wrap.style.webkitPerspective = scale * 4;
 
-        if (i == 1) {
+        if (i === 1) {
             dom.style.zIndex = scale - absoluteOffset;
-        } else {
+        }
+        else {
             dom.style.zIndex = (offset > 0) ? (1 - i) * absoluteOffset : (i - 1) * absoluteOffset;
         }
-        
+
         dom.style.backgroundColor = bdColor || '#333';
         dom.style.position = 'absolute';
         dom.style.webkitBackfaceVisibility = 'hidden';
         dom.style.webkitTransformStyle = 'preserve-3d'; 
-        dom.style.webkitTransform = 'rotate' + rotateDirect + '(' + 90 * (offset/scale + i - 1)+ 'deg) translateZ(' + (0.888 * scale/2) + 'px) scale(0.888)';
+        dom.style.webkitTransform = 'rotate' + rotateDirect + '(' + 90 * (offset/scale + i - 1) + 'deg) translateZ('
+                                    + (0.888 * scale/2) + 'px) scale(0.888)';
     },
 
     'flip': function (dom, axis, scale, i, offset) {
-        var rotateDirect = (axis == "X") ? "Y" : "X";
+        var rotateDirect = (axis === 'X') ? 'Y' : 'X';
         var bdColor = window.getComputedStyle(this.wrap.parentNode, null).backgroundColor;
-        if (this.isVertical){ offset = -offset; }
-
+        if (this.isVertical) { 
+            offset = -offset; 
+        }
         this.wrap.style.webkitPerspective = scale * 4;
 
         if (offset > 0) {
             dom.style.visibility = (i > 1) ? 'hidden' : 'visible';
-        } else {
+        } 
+        else {
             dom.style.visibility = (i < 1) ? 'hidden' : 'visible';
         }
 
         dom.style.backgroundColor = bdColor || '#333';
         dom.style.position = 'absolute';
         dom.style.webkitBackfaceVisibility = 'hidden';
-        dom.style.webkitTransform = 'translateZ('+ (scale/2) +'px) rotate' + rotateDirect + '(' + 180 * (offset/scale + i - 1)+ 'deg) scale(0.875)';
+        dom.style.webkitTransform = 'translateZ('+ (scale/2) +'px) rotate' + rotateDirect
+                                    + '(' + 180 * (offset/scale + i - 1)+ 'deg) scale(0.875)';
     },
 
     'depth': function (dom, axis, scale, i, offset) {
-        var rotateDirect = (axis == "X") ? "Y" : "X";
         var zoomScale = (4 - Math.abs(i - 1)) * 0.15;
 
         this.wrap.style.webkitPerspective = scale * 4;
 
-        if (i == 1) {
+        if (i === 1) {
             dom.style.zIndex = 100;
-        } else {
+        } 
+        else {
             dom.style.zIndex = (offset > 0) ? (1 - i) : (i - 1);
         }
 
-        dom.style.webkitTransform = 'scale('+ zoomScale + ', '+ zoomScale + ') translateZ(0) translate' + axis + '(' + (offset + 1.3 * scale * (i - 1)) + 'px)';
+        dom.style.webkitTransform = 'scale('+ zoomScale + ', '+ zoomScale + ') translateZ(0) translate' 
+                                    + axis + '(' + (offset + 1.3 * scale * (i - 1)) + 'px)';
     },
 
     'tear': function (dom, axis, scale, i, offset) {
-        var rotateDirect = (axis == "X") ? "Y" : "X";
+        var rotateDirect = (axis === 'X') ? 'Y' : 'X';
         var absoluteOffset = Math.abs(offset);
         
         this.wrap.style.webkitPerspective = scale * 4;
 
-        if (i == 1) {
+        if (i === 1) {
             dom.style.zIndex = scale - absoluteOffset;
             dom.cur = 1;
-        } else {
+        } 
+        else {
             dom.style.zIndex = (offset > 0) ? (1 - i) * absoluteOffset * 1000 : (i - 1) * absoluteOffset*1000;
         }
 
-        if (dom.cur && dom.cur != i){
+        if (dom.cur && dom.cur !== i){
             setTimeout(function(){
                 dom.cur = null
             },300)
@@ -193,11 +203,12 @@ iSlider.prototype._animateFuncs = {
 
         var zoomScale = (dom.cur) ? 1 - 0.2 * Math.abs(i-1) - Math.abs(0.2 * offset / scale).toFixed(6) : 1;
 
-        dom.style.webkitTransform = 'scale('+ zoomScale + ', '+ zoomScale + ') translateZ(0) translate' + axis + '(' + ((1 + Math.abs(i - 1) * 0.2) * offset + scale * (i - 1)) + 'px)';
+        dom.style.webkitTransform = 'scale('+ zoomScale + ', '+ zoomScale + ') translateZ(0) translate'
+                                    + axis + '(' + ((1 + Math.abs(i - 1) * 0.2) * offset + scale * (i - 1)) + 'px)';
     }
-}
+};
 
-//enable damping when slider meet the edge
+// enable damping when slider meet the edge
 iSlider.prototype._setUpDamping = function () {
     var oneIn2 = this.scale >> 1;
     var oneIn4 = oneIn2 >> 1;
@@ -209,9 +220,11 @@ iSlider.prototype._setUpDamping = function () {
 
         if (dis < oneIn2) {
             result = dis >> 1;
-        } else if (dis < oneIn2 + oneIn4) {
+        } 
+        else if (dis < oneIn2 + oneIn4) {
             result = oneIn4 + ((dis - oneIn2) >> 2);
-        } else {
+        } 
+        else {
             result = oneIn4 + oneIn16 + ((dis - oneIn2 - oneIn4) >> 3);
         }
 
@@ -219,24 +232,23 @@ iSlider.prototype._setUpDamping = function () {
     };
 };
 
-//render single item html by idx
+// render single item html by idx
 iSlider.prototype._renderItem = function (i) {
-    var item, html;
+    var item;
+    var html;
     var len = this.data.length;
-    var renderIndex = false;
 
     if (!this.isLooping) {
-        renderIndex = i;
-        item = this.data[i] || { empty : true };
-    } else {
+        item = this.data[i] || {empty: true};
+    } 
+    else {
         if (i < 0) {
-            renderIndex = len + i;
             item = this.data[len + i];
-        } else if (i > len - 1) {
-            renderIndex = i - len;
+        } 
+        else if (i > len - 1) {
             item = this.data[i - len];
-        } else {
-            renderIndex = i;
+        } 
+        else {
             item = this.data[i];
         }
     }
@@ -246,93 +258,97 @@ iSlider.prototype._renderItem = function (i) {
     }
 
     if (this.type === 'pic') {
-        html = item.height / item.width > this.ratio 
+        html = item.height / item.width > this.ratio
         ? '<img height="' + this.height + '" src="' + item.content + '">'
         : '<img width="' + this.width + '" src="' + item.content + '">';
-    } else if (this.type === 'dom') {
+    }
+    else if (this.type === 'dom') {
         html = '<div style="height:' + item.height + ';width:' + item.width + ';">' + item.content + '</div>';
-    } else if (this.type === 'overspread') {
+    }
+    else if (this.type === 'overspread') {
         html = this.ratio < 1 
-        ? '<div style="height: 100%; width:100%; background:url(' + item.content + ') center no-repeat; background-size:' + this.width + 'px auto;"></div>'
-        : '<div style="height: 100%; width:100%; background:url(' + item.content + ') center no-repeat; background-size: auto ' + this.height + 'px;"></div>';
+        ? '<div style="height: 100%; width:100%; background:url(' + item.content + ') center no-repeat; background-size:'
+            + this.width + 'px auto;"></div>'
+        : '<div style="height: 100%; width:100%; background:url(' + item.content + ') center no-repeat; background-size: auto '
+            + this.height + 'px;"></div>';
     }
 
     return html;
 };
 
-//render list html
+// render list html
 iSlider.prototype._renderHTML = function () {
     var outer;
 
     if (this.outer) {
-        //used for reset
+        // used for reset
         this.outer.innerHTML = '';
         outer = this.outer;
-    } else {
-        //used for initialization
+    } 
+    else {
+        // used for initialization
         outer = document.createElement('ul');
     }
 
-    //ul width equels to div#canvas width
+    // ul width equels to div#canvas width
     outer.style.width = this.width + 'px';
     outer.style.height = this.height + 'px';
 
-    //storage li elements, only store 3 elements to reduce memory usage
+    // storage li elements, only store 3 elements to reduce memory usage
     this.els = [];
     for (var i = 0; i < 3; i++) {
         var li = document.createElement('li');
         li.style.width = this.width + 'px';
         li.style.height = this.height + 'px';
 
-        //prepare style animation
+        // prepare style animation
         this._animateFunc(li, this.axis, this.scale, i, 0);
 
         this.els.push(li);
         outer.appendChild(li);
 
-        if (this.isVertical && (this._opts.animateType == 'rotate' || this._opts.animateType == 'flip')) {
+        if (this.isVertical && (this._opts.animateType === 'rotate' || this._opts.animateType === 'flip')) {
             li.innerHTML = this._renderItem(1 - i + this.sliderIndex);
-        } 
+        }
         else {
             li.innerHTML = this._renderItem(i - 1 + this.sliderIndex);
         }
     }
 
     this._preLoadImg(this.els);
-    
-    //append ul to div#canvas
+
+    // append ul to div#canvas
     if (!this.outer) {
         this.outer = outer;
         this.wrap.appendChild(outer);
     }
 };
 
-//get image size when the image is ready
+// get image size when the image is ready
 iSlider.prototype._getImgSize = function(img, index) {
 
     var self = this;
 
-    if (this.data[index].width == undefined 
-        || this.data[index].height == undefined) {
-        
+    if (this.data[index].width === undefined 
+        || this.data[index].height === undefined) {
+
         img.onload = function() {
             self.data[index].height = img.height;
             self.data[index].width = img.width;
-            console.log(self.data[index]);
-        }
+        };
     }
-}
+};
 
 // start loading image
 iSlider.prototype._startLoadingImg = function(index, direction) {
-    var dirIndex = (direction === 'right')? 1 : 0;
+    var dirIndex = (direction === 'right') ? 1 : 0;
     this.loadedImage[dirIndex] = new Image();
     this.loadedImage[dirIndex].src = this.data[index].content;
     this._getImgSize(this.loadedImage[dirIndex], index);
 
-}
+};
 
-//pre load image
+// pre load image
 iSlider.prototype._preLoadImg = function(els) {
 
     var self = this;
@@ -355,16 +371,14 @@ iSlider.prototype._preLoadImg = function(els) {
             if (dataLen - 2 !== 3 && self.isLooping) {
                 self._startLoadingImg(dataLen - 2, 'left');
             }
-            
         }
         else {
             self._preLoadImg(els);
         }
     }, 200);
-    
 };
 
-//logical slider, control left or right
+// logical slider, control left or right
 iSlider.prototype._slide = function (n) {
     var data = this.data;
     var dataLen = this.data.length;
@@ -377,16 +391,18 @@ iSlider.prototype._slide = function (n) {
         this._startLoadingImg(loadIndex, 'right');
     }
     else if (this.isLooping) {
-        loadIndex = (idx - 2 < 0 ) ? (dataLen - 2 + idx) : (idx - 2);
+        loadIndex = (idx - 2 < 0) ? (dataLen - 2 + idx) : (idx - 2);
         this._startLoadingImg(loadIndex, 'right');
     }
 
-    if (data[idx]){
+    if (data[idx]) {
         this.sliderIndex = idx;
-    } else {
+    }
+    else {
         if (this.isLooping) {
             this.sliderIndex = n > 0 ? 0 : data.length - 1;    
-        } else {
+        } 
+        else {
             n = 0;
         }
     }
@@ -394,15 +410,17 @@ iSlider.prototype._slide = function (n) {
     this.log('pic idx:' + this.sliderIndex);
 
     var sEle;
-    if ( this.isVertical && (this._opts.animateType == 'rotate' || this._opts.animateType == 'flip')) {
+    if ( this.isVertical && (this._opts.animateType === 'rotate' || this._opts.animateType === 'flip')) {
         if (n > 0) {
             sEle = els.pop();
             els.unshift(sEle);
-        } else if (n < 0) {
+        } 
+        else if (n < 0) {
             sEle = els.shift();
             els.push(sEle);
         }
-    } else {
+    } 
+    else {
         if (n > 0) {
             sEle = els.shift();
             els.push(sEle);
@@ -438,16 +456,14 @@ iSlider.prototype._slide = function (n) {
     }
 };
 
-//bind all event handler
+// bind all event handler
 iSlider.prototype._bindHandler = function () {
     var self = this;
-    var scaleW = self.scaleW;
     var outer = self.outer;
-    var len = self.data.length;
-    var isMoving = false;  // judge mousemove start or end
+    var isMoving = false;// judge mousemove start or end
 
     var hasTouch = (function () {
-        return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+        return !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof window.DocumentTouch);
     })();
 
     var startEvt = hasTouch ? 'touchstart' : 'mousedown';
@@ -496,20 +512,22 @@ iSlider.prototype._bindHandler = function () {
     var endHandler = function (evt) {
         evt.preventDefault();
 
-        isMoving = false; 
+        isMoving = false;
         var boundary = self.scale / 2;
         var metric = self.offset;
         var endTime = new Date().getTime();
 
-        //a quick slide time must under 300ms
-        //a quick slide should also slide at least 14 px
+        // a quick slide time must under 300ms
+        // a quick slide should also slide at least 14 px
         boundary = endTime - self.startTime > 300 ? boundary : 14;
 
         if (metric >= boundary) {
             self._slide(-1);
-        } else if (metric < -boundary) {
+        } 
+        else if (metric < -boundary) {
             self._slide(1);
-        } else {
+        } 
+        else {
             self._slide(0);
         }
 
@@ -532,15 +550,15 @@ iSlider.prototype._bindHandler = function () {
     window.addEventListener('orientationchange', orientationchangeHandler);
 };
 
-iSlider.prototype.reset = function () {
+iSlider.prototype.reset = function() {
     this.pause();
     this._setting();
     this._renderHTML();
     this.isAutoplay && this.play();
 };
 
-//enable autoplay
-iSlider.prototype.play = function () {
+// enable autoplay
+iSlider.prototype.play = function() {
     var self = this;
     var duration = this.duration;
     clearInterval(this.autoPlayTimer);
@@ -549,15 +567,15 @@ iSlider.prototype.play = function () {
     }, duration);
 };
 
-//pause autoplay
-iSlider.prototype.pause = function () {
+// pause autoplay
+iSlider.prototype.pause = function() {
     clearInterval(this.autoPlayTimer);
 };
 
-//plugin extend
-iSlider.prototype.extend = function(plugin){
+// plugin extend
+iSlider.prototype.extend = function(plugin) {
     var fn = iSlider.prototype;
     Object.keys(plugin).forEach(function(property) {
-        Object.defineProperty(fn, property, Object.getOwnPropertyDescriptor( plugin, property ) );
-    })
-}
+        Object.defineProperty(fn, property, Object.getOwnPropertyDescriptor(plugin, property));
+    });
+};
