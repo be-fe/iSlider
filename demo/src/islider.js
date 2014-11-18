6eb40dcbbea52de559bd694d35804cc26112b029
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2014-11-18 11:33:25 +0800

    fix rotate bug

--- demo/src/islider.js
+++ demo/src/islider.js
@@ -138,7 +138,8 @@ iSlider.prototype._animateFuncs = {
 
         dom.style.cssText += '-webkit-backface-visibility:hidden; -webkit-transform-style:preserve-3d; '
             + 'background-color:' + bdColor + '; position:absolute;';
-        dom.style.webkitTransform = 'rotate' + rotateDirect + '(' + 90 * (offset / scale + i - 1) + 'deg) translateZ(0.08) scale(0.888)';
+        dom.style.webkitTransform = 'rotate' + rotateDirect + '(' + 90 * (offset / scale + i - 1) + 'deg) translateZ('
+                                    + (0.81 * scale / 2) + 'px) scale(0.81)';
     },
 
     'flip': function (dom, axis, scale, i, offset) {
@@ -156,8 +157,8 @@ iSlider.prototype._animateFuncs = {
         }
 
         dom.style.cssText += 'position:absolute; -webkit-backface-visibility:hidden; background-color:' + bdColor + ';';
-        dom.style.webkitTransform = 'translateZ(0.80) rotate' + rotateDirect
-                                    + '(' + 180 * (offset / scale + i - 1) + 'deg) scale(0.875)';
+        dom.style.webkitTransform = 'translateZ(' + (scale / 2) + 'px) rotate' + rotateDirect
+                                    + '(' + 180 * (offset / scale + i - 1) + 'deg) scale(0.80)';
     },
 
     'depth': function (dom, axis, scale, i, offset) {
