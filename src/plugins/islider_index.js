d439ffc53fd16e27620eb98ad1789209e80ac236
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2014-11-19 10:31:20 +0800

    fix bad format

--- src/plugins/islider_index.js
+++ src/plugins/islider_index.js
@@ -1,8 +1,25 @@
+/**
+ * iSlider Dots Plugin
+ * A simple, efficent mobile slider
+ * @Author BEFE
+ *
+ * @param {Object}      menuOpts                参数集         Options
+ * @param {Boolean}     menuOpts.isVertical     垂直/居中      Veritcal/Horizontal     
+ * @param {String}      menuOpts.width          宽度           width
+ * @param {String}      menuOpts.height         高度           height
+ * @param {String}      menuOpts.top            距高           top position
+ * @param {String}      menuOpts.bottom         距底           bottom position
+ * @param {String}      menuOpts.left           距左           left position
+ * @param {String}      menuOpts.right          距右           right position
+ * @param {String}      menuOpts.diameter       直径           diameter of points
+ * @param {String}      menuOpts.borderColor    边框颜色        color of border
+ * @class
+ */
 iSlider.prototype.extend({
-    renderIndex: function(indexOpts) {
-        
-        this.indexOpts = indexOpts || {};
-        var pointMenuOpt = this.indexOpts.pointMenu || {};
+    renderIndex: function(menuOpts) {
+
+        this.menuOpts = menuOpts || {};
+        var pointMenuOpt = this.menuOpts || {};
         var data = this.data;
         var len = data.length;
         var wrap = this.wrap;
@@ -21,23 +38,25 @@ iSlider.prototype.extend({
             indexLayer.style.height = pointMenuOpt.height || '10%';
             indexLayer.style.marginLeft = 'auto';
             indexLayer.style.marginRight = 'auto';
+            indexLayer.style.textAlign = 'center';
         }
-            
+
+        indexLayer.style.padding = '0';
         indexLayer.style.position = 'absolute';
         indexLayer.style.left = pointMenuOpt.left || '0';
         indexLayer.style.right = pointMenuOpt.right || '0';
         indexLayer.style.top = pointMenuOpt.top || '0';
         indexLayer.style.bottom = pointMenuOpt.bottom || '0';
-        
+
         indexLayer.style.zIndex = '10000';
 
-        var node = wrap.parentNode.insertBefore(indexLayer, wrap.nextSibling);
+        wrap.parentNode.insertBefore(indexLayer, wrap.nextSibling);
         var fragment = document.createDocumentFragment();
         for (var i = 0; i < len; i++) {
             var point = document.createElement('li');
             point.id = 'point' + i;
-            point.style.height = '10px';
-            point.style.width = '10px';
+            point.style.height = pointMenuOpt.diameter || '1em';
+            point.style.width = pointMenuOpt.diameter || '1em';
             point.style.borderRadius = '50%';
             point.style.border = '1px solid';
             point.style.borderColor = pointMenuOpt.borderColor || '#fff';
@@ -46,7 +65,7 @@ iSlider.prototype.extend({
             point.style.margin = '5px';
 
             if (pointMenuOpt.isVertical && pointMenuOpt.isVertical === true) {
-                
+                point.style.display = 'block';
             }
             else {
                 point.style.display = 'inline-block';
@@ -57,14 +76,9 @@ iSlider.prototype.extend({
             if (i === 0) {
                 point.style.backgroundColor = pointMenuOpt.borderColor || '#fff';
             }
-
-            var self = this;
-            point.addEventListener('touchstart', function(evt) {
-                self.bindTouchEvent(evt, self);
-            } ,false);
-            point.addEventListener('click', function(evt) {
-                self.bindTouchEvent(evt, self);
-            } ,false);
+            point.self = this;
+            point.addEventListener('touchstart', this.bindTouchEvent, false);
+            point.addEventListener('click', this.bindTouchEvent, false);
             fragment.appendChild(point);
         }
 
@@ -74,7 +88,7 @@ iSlider.prototype.extend({
 
     changeIndex: function() {
 
-        var pointMenuOpt = this.indexOpts.pointMenu || {};
+        var pointMenuOpt = this.menuOpts || {};
         var idx = this.slideIndex;
         var data = this.data;
         var len = data.length;
@@ -88,10 +102,9 @@ iSlider.prototype.extend({
         }
     },
 
-    bindTouchEvent: function(evt, self) {
-        
+    bindTouchEvent: function(evt) {
+        var self = this.self;
         var idx = parseInt(evt.target.id.substring(5));
         self.slideTo(idx);
     }
-
 });
