fe23a367d0424153f00fa9d386de907bb44256be
xieyu33333 <xieyu33333@gmail.com>
2014-12-16 16:45:33 +0800

    add require

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp=require('gulp');
 
 gulp.task('default', function() {
-   gulp.watch(['src/islider.js', 'src/plugins/*.js'], function(){
-        return gulp.src(['src/islider.js', 'src/plugins/*.js'])
+   gulp.watch(['src/*.js', 'src/plugins/*.js'], function(){
+        return gulp.src(['src/*.js', 'src/plugins/*.js'])
                 .pipe(gulp.dest('demo/public/js'))
                 .pipe(gulp.dest('test/public/js'));
    });
 
-   gulp.watch(['src/islider.css'], function(){
-        return gulp.src(['src/islider.css'])
+   gulp.watch(['src/*.css'], function(){
+        return gulp.src(['src/*.css'])
                 .pipe(gulp.dest('demo/public/css'))
                 .pipe(gulp.dest('test/public/css'));
    });
