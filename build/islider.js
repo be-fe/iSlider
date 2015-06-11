b9886ae54203843699a4e52297b6d381816aae03
xieyu33333 <xieyu33333@gmail.com>
2015-06-11 18:04:48 +0800

    build

--- build/islider.js
+++ build/islider.js
@@ -565,9 +565,10 @@ iSlider = function () {
   *  orientationchange callback
   */
   iSlider.prototype.orientationchangeHandler = function () {
+    var self = this;
     setTimeout(function () {
-      this.reset();
-      this.log('Event: orientationchange');
+      self.reset();
+      self.log('Event: orientationchange');
     }, 100);
   };
   /**
