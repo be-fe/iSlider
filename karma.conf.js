357010aa69cca9905a301f2fd281bdff88cbfceb
WangRunshi <shinate@WangRunshideMacBook-Pro.local>
2016-01-27 18:16:31 +0800

    karma

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine'],
+        frameworks: ['mocha'],
 
 
         // list of files / patterns to load in the browser
@@ -64,7 +64,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: ['Chrome', 'PhantomJS'],
 
 
         // Continuous Integration mode
