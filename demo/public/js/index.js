5ba2937805ca542cd25dfc1d979344f0a0771486
lili39 <lili39@baidu.com>
2016-02-19 19:40:31 +0800

    demo页适配

--- demo/public/js/index.js
+++ demo/public/js/index.js
@@ -1,5 +1,9 @@
-(function ($) {
+(function () {
+    var t = document.getElementById("js-toggle-menu");
 
+    t.addEventListener('click', function () {
+        document.body.className = -1 !== document.body.className.indexOf("open-menu") ? "closed-menu": "open-menu";
+    })
     var hljs = window.hljs;
     var demoCode = $('#demo-code');
     var toggleBox = $('#toggle-box');
@@ -23,6 +27,13 @@
 
     $(document).ready(function () {
         setShowCode(frameSet.get(0));
+        $('.mobile-menu').click(function (e) {
+            e.stopPropagation();
+            $('.guide').addClass('open');
+        });
+        $(document).bind('click',function(){
+            $('.guide').removeClass('open');
+        });
     });
 
-})(jQuery);
+})();
