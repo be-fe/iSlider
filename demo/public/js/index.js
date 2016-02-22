5bc3164861ad94fffcd1e39df033f740e0382820
lili39 <gismanli@163.com>
2016-02-22 20:03:53 +0800

    demo add params tree

--- demo/public/js/index.js
+++ demo/public/js/index.js
@@ -1,8 +1,9 @@
 (function () {
     var t = document.getElementById("js-toggle-menu");
 
-    t.addEventListener('click', function () {
-        document.body.className = -1 !== document.body.className.indexOf("open-menu") ? "closed-menu": "open-menu";
+    t.addEventListener('click', function (e) {
+        // e.stopPropagation();
+        // document.body.className = -1 !== document.body.className.indexOf("open-menu") ? "closed-menu": "open-menu";
     })
     var hljs = window.hljs;
     var demoCode = $('#demo-code');
@@ -27,13 +28,60 @@
 
     $(document).ready(function () {
         setShowCode(frameSet.get(0));
-        $('.mobile-menu').click(function (e) {
+
+        $('.hamburger-menu').click(function (e) {
             e.stopPropagation();
-            $('.guide').addClass('open');
+            document.body.className = -1 !== document.body.className.indexOf("open-menu") ? "closed-menu": "open-menu";
         });
-        $(document).bind('click',function(){
-            $('.guide').removeClass('open');
+
+        var str = '';
+        $("#functions h4").each(function(index, element) {
+            str += '<li><a class="item" href="#'+element.innerText+'">'+element.innerText+'</a></li>'
+        }); 
+        $("#function-tree").html(str);
+
+        str = '';
+        $(".options-content h4").each(function(index, element) {
+            str += '<li><a class="item" href="#'+element.innerText+'">'+element.innerText+'</a></li>'
+        }); 
+        $("#options-param-tree").html(str);
+
+        str = '';
+        $("#method-static h4").each(function(index, element) {
+            str += '<li><a class="item" href="#'+element.innerText+'">'+element.innerText+'</a></li>'
+        }); 
+        $("#method-static-tree").html(str);
+
+        str = '';
+        $("#method-example h4").each(function(index, element) {
+            str += '<li><a class="item" href="#'+element.innerText+'">'+element.innerText+'</a></li>'
+        }); 
+        $("#method-example-tree").html(str);
+
+        $(".tree").each(function(index, element) { 
+            if($(this).next(".node").length>0){ 
+                $(this).addClass("ceng_close"); 
+            } 
         });
+
+        $('.item').click(function (e) {
+            document.body.className = -1 !== document.body.className.indexOf("open-menu") ? "closed-menu": "open-menu";
+        });
+
+        $(".tree").click(function(e){ 
+            var ul = $(this).next(".node");
+            if (ul.css("display")=="none"){ 
+                ul.slideDown(); 
+                $(this).addClass("ceng_open"); 
+                ul.find(".ceng_close").removeClass("ceng_open"); 
+            }
+            else { 
+                ul.slideUp(); 
+                $(this).removeClass("ceng_open"); 
+                ul.find(".node").slideUp(); 
+                ul.find(".ceng_close").removeClass("ceng_open"); 
+            } 
+        }); 
     });
 
 })();
