3542241b4ccc2f9ced8db234dc81fbd552b8d9a5
lili39 <gismanli@163.com>
2016-02-23 18:49:58 +0800

    delete pointer-event

--- demo/public/js/index.js
+++ demo/public/js/index.js
@@ -21,8 +21,6 @@ function setShowCode(iframe) {
 }
 
 $(document).ready(function () {
-    setShowCode(frameSet.get(0));
-
     $('.hamburger-menu').click(function (e) {
         document.body.className = -1 !== document.body.className.indexOf("open-menu") ? "closed-menu": "open-menu";
     });
